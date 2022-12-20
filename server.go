5c5ce7369091a9b2739ac4184f393d060fa91286
sing3demons <sing3demons@live.com>
2022-12-20 07:48:02 +0700

    :sparkles: init project

---
+++ server.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+func initDB() *sql.DB {
+	connStr := os.Getenv("DATABASE_URL")
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	createTb := `
+	CREATE TABLE IF NOT EXISTS expenses (
+		id SERIAL PRIMARY KEY,
+		title TEXT,
+		amount FLOAT,
+		note TEXT,
+		tags TEXT[]
+	);
+	`
+	_, err = db.Exec(createTb)
+	if err != nil {
+		log.Fatal("can't create table", err)
+	}
+
+	return db
+}
+
+func main() {
+
+	e := echo.New()
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	// e.POST("/expenses", CreateExpensesHandler)
+	// e.GET("/expenses/:id", GetExpensesHandlerByID)
+	// e.PUT("/expenses/:id", UpdateExpensesHandler)
+	// e.DELETE("/expenses/:id", DeleteExpenseHandlerByID)
+	// e.GET("/expenses", ListExpensesHandler)
+	// e.GET("/expenses", ListExpensesHandler)
+
+	fmt.Println("start at port:", os.Getenv("PORT"))
+	go func() {
+		if err := e.Start(":" + os.Getenv("PORT")); err != nil && err != http.ErrServerClosed {
+			e.Logger.Fatal("shutting down the server")
+		}
+	}()
+
+	shutdown := make(chan os.Signal, 1)
+	signal.Notify(shutdown, os.Interrupt, syscall.SIGTERM)
+	<-shutdown
+	fmt.Println("shutting down...")
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	if err := e.Shutdown(ctx); err != nil {
+		e.Logger.Fatal(err)
+	}
+	fmt.Println("server stop")
+}
